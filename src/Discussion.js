60f5c1b42bdd7d8425c20da99db3d9c7842a8d01
Mayank Jain <mayankjain@mayanks-mbp-2.home>
2021-05-10 20:27:01 -1000

    squash

--- src/Discussion.js
+++ src/Discussion.js
@@ -3,22 +3,33 @@ import { useState } from 'react';
 import Avatar1 from './Slice_1.png';
 
 function Emoji(props) {
-  const [ isChecked, setIsChecked ] = useState(false);
+  const [ isReplying, setIsReplying ] = useState(false);
 
   return (
     <div className="discussion">
       <div className="question">{ props.text }</div>
-      <div><a href="#" className="reply-button">Reply</a></div>
-      <div className="reply-box-div">
-        <textarea className="reply-box"></textarea>
-        <button className="submit-button">Submit</button>
-      </div>
-      <div className="reply">
-        <div className="author-header">
-          <img src={ Avatar1 } />
-          <div className="author-name">Mayank</div>
+      <div><a onClick={ () => setIsReplying(true) } className="reply-button">Reply</a></div>
+      {
+        isReplying &&
+        <div className="reply">
+          <div className="reply-flex">
+            <div className="author-header">
+              <img src={ Avatar1 } />
+              <div className="author-name">Mayank</div>
+            </div>
+            <input contentEditable autoFocus className="doc-block reply-input"/>
+          </div>
+          <a href="#" className="reply-button">Reply</a>
         </div>
-        <div>I think just wait a week, I bet they'll be over it really fast</div>
+      }
+      <div className="reply">
+        <div className="reply-flex">
+          <div className="author-header">
+            <img src={ Avatar1 } />
+            <div className="author-name">Mayank</div>
+          </div>
+          <div contentEditable className=" doc-block reply-input">I think just wait a week, I bet they'll be over it really fast</div>
+        </div>
         <a href="#" className="reply-button">Reply</a>
       </div>
     </div>
--- src/assigned.js
+++ src/assigned.js
@@ -1,15 +1,18 @@
 import './assigned.css';
+import EmailIcon from './email-icon.png';
 import { useState } from 'react';
 
 function Emoji(props) {
   const [ isChecked, setIsChecked ] = useState(false);
 
   return (
-    <div>
+    <div className="assigned">
       {
         isChecked
         ? <input autoFocus className="fake-input"/>
-        : <button className="input-requested" onClick={ () => setIsChecked(!isChecked) }>Input requested from {props.from}</button>
+        : <button className="input-requested" onClick={ () => setIsChecked(!isChecked) }>
+            Input requested from {props.from} <img className="email-icon" src={ EmailIcon } />
+          </button>
       }
     </div>
   );
